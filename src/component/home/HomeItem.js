23af76e12845ebf7358b2a3054863d2b95630bd0
FaturRumahan <81752796+faturrumahan@users.noreply.github.com>
2023-11-13 21:15:27 +0700

    Update HomeItem.js

--- src/component/home/HomeItem.js
+++ src/component/home/HomeItem.js
@@ -7,7 +7,7 @@ const HomeItem = () => {
       <div className="flex flex-wrap justify-center items-center h-screen xl:justify-start">
         <div className="text-center xl:text-start">
           <h1 className="text-white block font-bold text-5xl md:text-7xl">
-            Hi, I' am Fatur
+            Hi, I am Fatur
           </h1>
           <h3 className="text-white block font-light text-l md:text-2xl">
             Undergraduate Informatics Student
